c40c9e7e80cb330fb711f79aa8a835082b5f4f10
Scion-Kin <dinturner17@gmail.com>
2024-07-03 21:28:08 +0200

    Task 0 done

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,8 +5,8 @@ from typing import List
 import re
 
 
-def filter_datum(fields: List[str], redaction: str, text: str, separator: str):
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str):
     ''' returns the log message with specified fields obfuscated '''
     for fi in fields:
-        text = re.sub(r'{}=.+?{}'.format(fi, separator), r'{}={}{}'.format(fi, redaction, separator), text)
-    return text
+        message = re.sub(r'{}=.+?{}'.format(fi, separator), r'{}={}{}'.format(fi, redaction, separator), message)
+    return message
