7c105e07ebc7d8198be315e9e1647cadf58b60a6
Scion-Kin <dinturner17@gmail.com>
2024-07-03 21:34:25 +0200

    Task 0 done

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,13 +4,10 @@
 from typing import List
 import re
 
-patterns = {
-    'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
-    'replace': lambda x: r'\g<field>={}'.format(x),
-}
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str):
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str):
     ''' returns the log message with specified fields obfuscated '''
-
-    extract, replace = (patterns["extract"], patterns["replace"])
-    return re.sub(extract(fields, separator), replace(redaction), message)
+    for fi in fields:
+        message = re.sub(r'{}=[^{}]*'.format(fi, separator), r'{}={}'.format(fi, redaction), message)
+    return message
