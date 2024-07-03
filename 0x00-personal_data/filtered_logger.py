f3620cf935ebb7f2d990b5e4aaa8f736aca0b446
Scion-Kin <dinturner17@gmail.com>
2024-07-03 22:18:09 +0200

    Task 0 done

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,6 +2,7 @@
 ''' This defines a function '''
 
 from typing import List
+import logging
 import re
 
 
@@ -11,3 +12,22 @@ def filter_datum(fields: List[str], redaction: str,
     for fi in fields:
         message = re.sub(r'{}=[^{}]*'.format(fi, separator), r'{}={}'.format(fi, redaction), message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        ''' formats incoming logs '''
+        msg = super(RedactingFormatter, self).format(record)
+        txt = filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
+        return txt
