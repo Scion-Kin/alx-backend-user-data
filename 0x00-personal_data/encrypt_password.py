9f6d0319aff3053e13764bc4c8abaabca343623e
Scion-Kin <dinturner17@gmail.com>
2024-07-04 21:09:28 +0200

    Task 6 done

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 ''' This defines functions '''
 
-from bcrypt import gensalt, hashpw
+from bcrypt import gensalt, hashpw, checkpw
 
 
 def hash_password(password: str) -> bytes:
     ''' returns a hashed password '''
 
     return hashpw(password.encode('utf-8'), gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    ''' checks if a password matches the hashed password '''
+
+    return checkpw(password.encode('utf-8'), hashed_password)
