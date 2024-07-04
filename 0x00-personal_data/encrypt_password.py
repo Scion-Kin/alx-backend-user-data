a215595f930c28a03b91cce53a042cb2385fe69d
Scion-Kin <dinturner17@gmail.com>
2024-07-04 20:54:25 +0200

    Task 5 done

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+''' This defines functions '''
+
+from bcrypt import gensalt, hashpw
+
+
+def hash_password(password: str) -> bytes:
+    ''' hashes a password '''
+
+    return hashpw(password.encode('utf-8'), gensalt())
