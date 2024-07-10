e9ed1505aac1478b84a94db799b9919e1ca135da
Scion-Kin <dinturner17@gmail.com>
2024-07-10 21:28:42 +0200

    Task 4 done

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,8 +10,12 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         ''' Checks if the requested path is in the excluded paths '''
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
 
-        return False
+        path = path + '/' if not path.endswith('/') else path
+
+        return True if path not in excluded_paths else False
 
     def authorization_header(self, request=None) -> str:
         ''' checks for an auth token in the request's header '''
