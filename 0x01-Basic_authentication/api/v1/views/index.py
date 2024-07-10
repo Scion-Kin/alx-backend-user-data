decc8f338a8586a31bb2f006448214810a469c16
Scion-Kin <dinturner17@gmail.com>
2024-07-10 21:06:48 +0200

    Task 2 done

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -31,3 +31,10 @@ def unauthorized() -> None:
     ''' aborts the request with an unauthorized code '''
 
     abort(401)
+
+
+@app_views.route('/forbidden', strict_slashes=False)
+def forbidden() -> None:
+    ''' aborts on a forbidden request '''
+
+    abort(403)
