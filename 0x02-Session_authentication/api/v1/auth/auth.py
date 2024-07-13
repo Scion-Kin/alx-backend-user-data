76e2563d1baf9a4bc1342294d0d4e9d09ca8918d
Scion-Kin <dinturner17@gmail.com>
2024-07-13 19:35:08 +0200

    Task 0 done

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+''' This defines a class '''
+
+import re
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    ''' Class responsible for authentication '''
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        ''' Checks if the requested path is in the excluded paths '''
+
+        if path is not None and excluded_paths is not None:
+            for exclusion_path in map(lambda x: x.strip(), excluded_paths):
+                pattern = ''
+                if exclusion_path[-1] == '*':
+                    pattern = '{}.*'.format(exclusion_path[0:-1])
+                elif exclusion_path[-1] == '/':
+                    pattern = '{}/*'.format(exclusion_path[0:-1])
+                else:
+                    pattern = '{}/*'.format(exclusion_path)
+                if re.match(pattern, path):
+                    return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        ''' checks for an auth token in the request's header '''
+
+        auth_header = request.headers.get('Authorization')
+
+        return None if request is None or not auth_header else auth_header
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        ''' verifies the user '''
+
+        return None
