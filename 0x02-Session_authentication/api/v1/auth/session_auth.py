2ca58b1b00055cee50a155a10997412ff4acabdf
Scion-Kin <dinturner17@gmail.com>
2024-07-13 19:58:26 +0200

    Task 2 done

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -12,6 +12,8 @@ class SessionAuth(Auth):
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
+        ''' creates a session for a user_id '''
+
         if user_id is None or type(user_id) is not str:
             return None
 
