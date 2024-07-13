0f0e2c0c882ef8dfb34f1b0d5244ec293ae05401
Scion-Kin <dinturner17@gmail.com>
2024-07-13 19:57:20 +0200

    Task 2 done

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,7 +3,19 @@
 
 from flask import request
 from .auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     ''' Class responsible for authentication '''
+
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        if user_id is None or type(user_id) is not str:
+            return None
+
+        ses_id = str(uuid4())
+        SessionAuth.user_id_by_session_id[ses_id] = user_id
+
+        return ses_id
