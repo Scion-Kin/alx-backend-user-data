35b103f7f65b0e5679ae1e9ddb652834c96e92ba
Scion-Kin <dinturner17@gmail.com>
2024-07-16 11:37:15 +0200

    Task 1 done

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -32,7 +32,7 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         ''' creates a new user object '''
-        if email is None or hashed_password is None:
+        if not email or not hashed_password:
             return
 
         user = User(email=email, hashed_password=hashed_password)
