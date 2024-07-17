0acfd0f83f4b11d9c00a545a893dd1e76e2a3c66
Scion-Kin <dinturner17@gmail.com>
2024-07-17 17:24:45 +0200

    Task 2 done

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -46,11 +46,11 @@ class DB:
 
         for key, val in kwargs.items():
             if not hasattr(User, key):
-                raise InvalidRequestError
+                raise InvalidRequestError()
 
         user = self._session.query(User).filter_by(**kwargs).first()
 
         if user is None:
-            raise NoResultFound
+            raise NoResultFound()
 
         return user
