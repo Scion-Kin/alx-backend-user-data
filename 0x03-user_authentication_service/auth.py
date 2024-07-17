a924ec56527473d0654b3b46a8fcaaa13ccb021b
Scion-Kin <dinturner17@gmail.com>
2024-07-17 20:13:52 +0200

    Task 13 done

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -70,3 +70,8 @@ class Auth:
 
         except NoResultFound:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        ''' deletes the session id from a User object '''
+
+        self._db.update_user(user_id, session_id=None)
