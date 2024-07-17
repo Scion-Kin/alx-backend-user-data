c39faba230de81c2295e3fcc7807da0970c5e9aa
Scion-Kin <dinturner17@gmail.com>
2024-07-17 18:00:58 +0200

    Task 5 done

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,9 +2,35 @@
 ''' Defines a function '''
 
 from bcrypt import hashpw, gensalt
+from db import DB
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
     ''' Hashes a password and returns the hashed value '''
 
     return hashpw(password.encode('utf-8'), gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        ''' makes and stores a new User object '''
+
+        try:
+            user = self._db.find_user_by(email=email)
+            raise ValueError('User {} already exists'.format(email))
+
+        except ValueError:
+            raise
+
+        except Exception:
+            user = User(email=email, hashed_password=_hash_password(password))
+
+            self._db._session.add(user), self._db._session.commit()
+            return
